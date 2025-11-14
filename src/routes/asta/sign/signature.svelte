762d98d01d291f46c915410963d48334015c4957
pondrax <pondras.setiawan@gmail.com>
2025-11-14 11:03:28 +0700

    Fix prevent redraw canvas

--- src/routes/asta/sign/signature.svelte
+++ src/routes/asta/sign/signature.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { createId, generateQRCode } from "$lib/utils";
-  // import { nanoid } from "nanoid";
   import type { SignatureType } from "./types";
   type Visual = "image" | "qr" | "box" | "draw";
 
@@ -28,32 +27,15 @@
   let ctxPad = $state() as CanvasRenderingContext2D;
   let ctxQR = $state() as CanvasRenderingContext2D;
 
+  let drawing = $state(false);
+
   const canvasConfigs = {
-    image: {
-      originX: 10,
-      originY: 10,
-      width: 128,
-      height: 128,
-    },
-    qr: {
-      originX: 350,
-      originY: 630,
-      width: 100,
-      height: 100,
-    },
-    box: {
-      originX: 310,
-      originY: 630,
-      width: 200,
-      height: 80,
-    },
-    draw: {
-      originX: 310,
-      originY: 630,
-      width: 200,
-      height: 100,
-    },
+    image: { originX: 10, originY: 10, width: 128, height: 128 },
+    qr: { originX: 350, originY: 630, width: 100, height: 100 },
+    box: { originX: 310, originY: 630, width: 200, height: 80 },
+    draw: { originX: 310, originY: 630, width: 200, height: 100 },
   };
+
   let canvasMap = $derived({
     image: canvasImage,
     qr: canvasQR,
@@ -61,37 +43,70 @@
     box: canvasBox,
   });
 
-  let drawing = $state(false);
-
-  const initCanvas = () => {
+  const initCanvas = async () => {
     if (!canvasImage || !canvasPad || !canvasBox || !canvasQR) return;
-    ctxImage = canvasImage.getContext("2d") as CanvasRenderingContext2D;
-    ctxBox = canvasBox.getContext("2d") as CanvasRenderingContext2D;
-    ctxPad = canvasPad.getContext("2d") as CanvasRenderingContext2D;
-    ctxQR = canvasQR.getContext("2d") as CanvasRenderingContext2D;
+
+    ctxImage = canvasImage.getContext("2d")!;
+    ctxBox = canvasBox.getContext("2d")!;
+    ctxPad = canvasPad.getContext("2d")!;
+    ctxQR = canvasQR.getContext("2d")!;
 
     ctxBox.font = "20px Arial";
     ctxBox.lineWidth = 5;
 
     ctxPad.font = "20px Arial";
     ctxPad.lineWidth = 3;
+
+    clearPad();
+    await drawStaticLayers();
+    await loadImageSignature();
+  };
+
+  const clearPad = () => {
     ctxPad.fillStyle = "#fff";
     ctxPad.fillRect(0, 0, canvasPad.width, canvasPad.height);
-    drawText();
-    imageLoader();
+    ctxPad.fillStyle = "#000";
+    ctxPad.textAlign = "center";
+    ctxPad.textBaseline = "middle";
+    ctxPad.fillText(form?.nama || "", canvasPad.width / 2, 340);
+  };
+
+  const drawStaticLayers = async () => {
+    // Draw Signature Box
+    ctxBox.fillStyle = "#fff";
+    ctxBox.fillRect(0, 0, canvasBox.width, canvasBox.height);
+    ctxBox.strokeRect(0, 0, canvasBox.width, canvasBox.height);
+    ctxBox.drawImage(logo, 20, 25, 140, 140);
+    ctxBox.fillStyle = "#000";
+    ctxBox.fillText("Ditandatangani secara elektronik oleh:", 180, 30);
+    ctxBox.fillText(form?.jabatan || "", 180, 60);
+    ctxBox.fillText(form?.instansi || "", 180, 90);
+    ctxBox.fillText(form?.nama || "", 180, 150);
+
+    // Draw Signature Pad
+    ctxPad.fillStyle = "#fff";
+    ctxPad.fillRect(0, 290, 640, 100);
+    ctxPad.fillStyle = "#000";
+    ctxPad.textAlign = "center";
+    ctxPad.textBaseline = "middle";
+    ctxPad.fillText(form?.nama || "", 320, 300);
+
+    // Draw QR CODE
+    const imgQR = await createImageBitmap(
+      (await generateQRCode(
+        { data: `${form?.nama}\n${form?.instansi}` },
+        true,
+      )) as Blob,
+    );
+    ctxQR.clearRect(0, 0, canvasQR.width, canvasQR.height);
+    ctxQR.drawImage(imgQR, 0, 0, canvasQR.width, canvasQR.height);
   };
 
   const getMousePosition = (event: MouseEvent | TouchEvent) => {
-    const rect = canvasPad?.getBoundingClientRect() || {
-      left: 0,
-      top: 0,
-      width: 1,
-      height: 1,
-    };
-    const scaleX = (canvasPad?.width || 1) / rect.width;
-    const scaleY = (canvasPad?.height || 1) / rect.height;
+    const rect = canvasPad.getBoundingClientRect();
+    const scaleX = canvasPad.width / rect.width;
+    const scaleY = canvasPad.height / rect.height;
 
-    // Handle touch events
     if (event instanceof TouchEvent) {
       const touch = event.touches[0] || event.changedTouches[0];
       return {
@@ -100,7 +115,6 @@
       };
     }
 
-    // Handle mouse events
     return {
       x: (event.clientX - rect.left) * scaleX,
       y: (event.clientY - rect.top) * scaleY,
@@ -112,7 +126,6 @@
     const { x, y } = getMousePosition(event);
     ctxPad.beginPath();
     ctxPad.moveTo(x, y);
-    // event.preventDefault(); // Prevent scrolling on touch devices
   };
 
   const draw = (event: MouseEvent | TouchEvent) => {
@@ -120,8 +133,6 @@
     const { x, y } = getMousePosition(event);
     ctxPad.lineTo(x, y);
     ctxPad.stroke();
-    drawText();
-    // event.preventDefault(); // Prevent scrolling on touch devices
   };
 
   const stopDrawing = () => {
@@ -129,50 +140,13 @@
     ctxPad.closePath();
   };
 
-  const drawText = async () => {
-    // Draw Signature Box
-    ctxBox.fillStyle = "#fff";
-    ctxBox.fillRect(0, 0, canvasBox.width, canvasBox.height);
-    ctxBox.strokeRect(0, 0, canvasBox.width, canvasBox.height);
-    ctxBox.drawImage(logo, 20, 25, 140, 140);
-    ctxBox.fillStyle = "#000";
-    ctxBox.fillText("Ditandatangani secara elektronik oleh:", 180, 30);
-    ctxBox.fillText(form?.jabatan || "", 180, 60);
-    ctxBox.fillText(form?.instansi || "", 180, 90);
-    ctxBox.fillText(form?.nama || "", 180, 150);
-    // ctxBox.fillText(form?.pangkat || '', 200, 180);
-
-    // Draw Signature Pad
-    ctxPad.fillStyle = "#fff";
-    ctxPad.fillRect(0, 290, 640, 100);
-    ctxPad.fillStyle = "#000";
-    ctxPad.textAlign = "center";
-    ctxPad.textBaseline = "middle";
-    ctxPad.fillText(form?.nama || "", 320, 300);
-    // ctxPad.fillText(form?.pangkat || '', 50, 300);
-
-    // Draw QR CODE
-    const imgQR = await createImageBitmap(
-      (await generateQRCode(
-        {
-          data: `${form?.nama}\n${form?.instansi}`,
-          // data: `${baseURL}/d?=${form?.id}`
-        },
-        true,
-      )) as Blob,
-    );
-    ctxQR.drawImage(imgQR, 0, 0, canvasQR?.width, canvasQR?.height);
-  };
-
   async function addSignature() {
     const canvas = canvasMap[visual];
     const config = canvasConfigs[visual];
-
     const imageBase64 = canvas
       .toDataURL()
-      ?.replace("data:image/png;base64,", "");
+      .replace("data:image/png;base64,", "");
 
-    // signatures.push({
     setSignature({
       id: createId(6),
       imageBase64,
@@ -187,32 +161,28 @@
     });
   }
 
-  async function imageLoader() {
-    if (imageSignature && imageSignature.length > 0) {
-      const file = imageSignature[0];
-      const reader = new FileReader();
-
-      reader.onload = () => {
-        const img = new Image();
-        img.onload = () => {
-          canvasImage.width = img.width;
-          canvasImage.height = img.height;
-          ctxImage?.drawImage(img, 0, 0);
-        };
-        img.src = reader.result as string;
+  async function loadImageSignature() {
+    if (!imageSignature || imageSignature.length === 0) return;
+    const file = imageSignature[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      const img = new Image();
+      img.onload = () => {
+        canvasImage.width = img.width;
+        canvasImage.height = img.height;
+        ctxImage.drawImage(img, 0, 0);
       };
-
-      reader.readAsDataURL(file);
-    }
+      img.src = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
 
   $effect(() => {
-    if (form) {
-      initCanvas();
-    }
+    if (form) initCanvas();
   });
+
   $effect(() => {
-    if (availableVisual.length > 0 && !availableVisual.includes(visual)) {
+    if (availableVisual.length && !availableVisual.includes(visual)) {
       visual = availableVisual[0];
     }
   });
