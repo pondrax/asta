015827196b4863d32b9e0d28dd1454de0219457c
pondrax <pondras.setiawan@gmail.com>
2025-12-18 00:51:15 +0700

    optional sed fix

--- fix.sh
+++ fix.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sed -i.bak.js 's/request_origin !== url.origin/!request_origin.includes(url.host)/g' build/server/index.js
+if [ -f "build/server/index.js" ]; then
+    sed -i.bak.js 's/request_origin !== url.origin/!request_origin.includes(url.host)/g' build/server/index.js
+fi
